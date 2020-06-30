d0f9e1f985d6104056193942b6732ea2e2892c9d
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-29 21:02:30 -0500

    started new spring course

---
+++ src/main/java/com/codeup/springcourse/springdemo1/Coach.java
@@ -0,0 +1,7 @@
+package com.codeup.springcourse.springdemo1;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+}
---
+++ src/main/java/com/codeup/springcourse/springdemo1/TrackCoach.java
@@ -0,0 +1,20 @@
+package com.codeup.springcourse.springdemo1;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
